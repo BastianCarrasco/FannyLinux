e0221c61be0c9a9fc1e7c45cb8683871fb77852c
BastianCarrasco <bastian.carrasco.v@mail.pucv.cl>
2024-02-23 10:28:49 -0300

    codigo de arranque y encargos

--- frontend/src/Vistas/Cola.jsx
+++ frontend/src/Vistas/Cola.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Importa Axios si aún no lo has hecho
-import { obtenerDatosPedidos} from '../funciones backend/consultas';
+import { obtenerDatosPedidos } from '../funciones backend/consultas';
 
 function Cola() {
   const [pedidos, setPedidos] = useState([]);
@@ -23,23 +23,23 @@ function Cola() {
     console.log(inputValue);
     const barraEnPedidos = pedidos.find(pedido => pedido.Barra === parseInt(inputValue));
     console.log(Boolean(barraEnPedidos));
-  
+
     if (Boolean(barraEnPedidos)) {
       axios.delete('http://localhost:5150/eliminar-pedidos-barra', {
         data: {
           barra: inputValue
         }
       })
-      .then(response => {
-        console.log(response.data); // Mensaje de éxito
-        // Actualizar los pedidos después de eliminar el pedido
-        setPedidos(pedidos.filter(pedido => pedido.Barra !== inputValue));
-        // Limpiar el valor del input después de eliminar el pedido
-        setInputValue('');
-      })
-      .catch(error => {
-        console.error('Error al eliminar pedidos:', error); // Manejo de errores
-      });
+        .then(response => {
+          console.log(response.data); // Mensaje de éxito
+          // Actualizar los pedidos después de eliminar el pedido
+          setPedidos(pedidos.filter(pedido => pedido.Barra !== inputValue));
+          // Limpiar el valor del input después de eliminar el pedido
+          setInputValue('');
+        })
+        .catch(error => {
+          console.error('Error al eliminar pedidos:', error); // Manejo de errores
+        });
     }
 
     async function fetchPedidos() {
@@ -56,10 +56,10 @@ function Cola() {
 
 
   }, [inputValue, pedidos]);
-  
-  
-  
-  
+
+
+
+
 
   useEffect(() => {
     const input = document.getElementById('inputBarra');
@@ -72,72 +72,72 @@ function Cola() {
     setInputValue(e.target.value);
   };
 
-// Función para determinar si un texto contiene alguna de las palabras específicas
-const contienePalabraEspecifica = (texto, palabras) => {
-  return palabras.some(palabra => texto.toLowerCase().includes(palabra.toLowerCase()));
-};
+  // Función para determinar si un texto contiene alguna de las palabras específicas
+  const contienePalabraEspecifica = (texto, palabras) => {
+    return palabras.some(palabra => texto.toLowerCase().includes(palabra.toLowerCase()));
+  };
 
 
-  
+
 
   return (
-    <div>
-      <h2>Cola de Pedidos</h2>
-      <input
-        id="inputBarra"
-        type="number"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Ingrese el número de barra"
-      />
-     
-      <table style={{ width: '100%' }}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Orden</th>
-            <th>Cantidad</th>
-            <th>Llaves</th>
-            <th>Comentario</th>
-            <th>Precio</th>
-            <th>Estado</th>
-            <th>Barra</th>
-            <th>Cliente</th>
-            <th>Número de Orden</th>
+    <div style={{ backgroundColor: "black", color: "yellow" }}>
+
+      <div style={{ display: "flex", textAlign: "center" }}>
+        <h2 style={{ marginRight: "20px" }}>Ingrese codigo de barra</h2>
+        <input
+          id="inputBarra"
+          type="number"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Ingrese el número de barra"
+        /></div>
+
+
+
+      <table style={{ width: '100%', border: "solid" }}>
+        <thead style={{ border: "solid" }}>
+          <tr style={{ border: "solid" }}>
+            {/* <th>ID</th> */}
+            <th style={{ border: "solid" }}>Orden</th>
+            <th style={{ border: "solid" }}>Cantidad</th>
+            {/* <th>Llaves</th> */}
+            <th style={{ border: "solid" }}>Comentario</th>
+            <th style={{ border: "solid" }}>Precio</th>
+            {/* <th>Estado</th> */}
+            {/* <th>Barra</th> */}
+            {/* <th>Cliente</th> */}
+            <th style={{ border: "solid" }}>Número de Orden</th>
           </tr>
         </thead>
-      <tbody>
-  {pedidos.filter(pedido => pedido.Estado === 0).map((pedido, index) => (
-    <tr key={index}>
-      <td>{pedido.Id_pedidos}</td>
-      <td>
-  {/* Dividir la orden en fragmentos y aplicar estilos si contiene alguna de las palabras específicas */}
-  {pedido.OrdenTxt.split(' ').map((fragmento, i) => (
-    contienePalabraEspecifica(fragmento, ['PapasFritasC', 'PapasFritasG', 'QuesCamFrita', 'Sopaipilla', 'Quesofrita','PesFrito']) ? 
-    <span key={i} style={{ color: 'red' }}>{fragmento} </span> : 
-    <span key={i}>{fragmento} </span>
-  ))}
-</td>
+        <tbody style={{ border: "solid" }}>
+          {pedidos.filter(pedido => pedido.Estado === 0 || pedido.Estado === 2).map((pedido, index) => (
+            <tr key={index} style={{ backgroundColor: pedido.Estado === 2 ? 'darkblue' : 'transparent' }}>
+              {/* <td>{pedido.Id_pedidos}</td> */}
+              <td style={{ border: "solid" }}>
+                {/* Dividir la orden en fragmentos y aplicar estilos si contiene alguna de las palabras específicas */}
+                {pedido.OrdenTxt.split(' ').map((fragmento, i) => (
+                  contienePalabraEspecifica(fragmento, ['PapasFritasC', 'PapasFritasG', 'QuesCamFrita', 'Sopaipilla', 'Quesofrita', 'PesFrito']) ?
+                    <span key={i} style={{ color: 'red' }}>{fragmento} </span> :
+                    <span key={i}>{fragmento} </span>
+                ))}
+              </td>
 
-      <td>{pedido.Cantidad}</td>
-      <td>{pedido.Llaves}</td>
-      <td>
+              <td style={{ border: "solid" }}>{pedido.Cantidad}</td>
+              {/* <td>{pedido.Llaves}</td> */}
+              <td style={{ border: "solid" }}>
                 {pedido.Comentario.split('\n').map((linea, i) => (
                   <div key={i}>{linea}</div>
                 ))}
               </td>
-      <td>{pedido.Precio}</td>
-      <td>{pedido.Estado}</td>
-      <td>{pedido.Barra}</td>
-      <td>{pedido.Cliente}</td>
-       {index === 0 || pedidos[index - 1].NumOrden !== pedido.NumOrden ? (
-          <td>{pedido.NumOrden}</td>
-        ) : (
-          <td style={{ visibility: 'hidden' }}>{pedido.NumOrden}</td>
-        )}
-    </tr>
-  ))}
-</tbody>
+              <td style={{ border: "solid" }}>{pedido.Precio}</td>
+              {/* <td>{pedido.Estado}</td> */}
+              {/* <td>{pedido.Barra}</td> */}
+              {/* <td>{pedido.Cliente}</td> */}
+              <td style={{ border: "solid" }}>{pedido.NumOrden}</td>
+            </tr>
+          ))}
+        </tbody>
 
       </table>
     </div>
--- frontend/src/Vistas/Encargos.jsx
+++ frontend/src/Vistas/Encargos.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // Importa Axios si aún no lo has hecho
-import { obtenerDatosPedidos} from '../funciones backend/consultas';
+import axios from 'axios'; 
+import { obtenerDatosPedidos } from '../funciones backend/consultas';
+import { FaUndoAlt } from 'react-icons/fa';
+import Modal from 'react-modal';
 
-function Encargos() {const [pedidos, setPedidos] = useState([]);
+function Encargos() {
+  const [pedidos, setPedidos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedPedido, setSelectedPedido] = useState(null);
+  const [numeroOrdenSeleccionado, setNumeroOrdenSeleccionado] = useState(null);
+  const [pedidosConNumeroOrden, setPedidosConNumeroOrden] = useState([]);
+  const [numerosOrdenMostrados, setNumerosOrdenMostrados] = useState([]);
+  const [rowSpanCount, setRowSpanCount] = useState(1); // Contador de filas fusionadas
 
   useEffect(() => {
     async function fetchPedidos() {
@@ -18,111 +27,133 @@ function Encargos() {const [pedidos, setPedidos] = useState([]);
     fetchPedidos();
   }, []);
 
-  useEffect(() => {
-    console.log(inputValue);
-    const barraEnPedidos = pedidos.find(pedido => pedido.Barra === parseInt(inputValue));
-    console.log(Boolean(barraEnPedidos));
-  
-    if (Boolean(barraEnPedidos)) {
-      axios.delete('http://localhost:5150/eliminar-pedidos-barra', {
-        data: {
-          barra: inputValue
-        }
-      })
-      .then(response => {
-        console.log(response.data); // Mensaje de éxito
-        // Actualizar los pedidos después de eliminar el pedido
-        setPedidos(pedidos.filter(pedido => pedido.Barra !== inputValue));
-        // Limpiar el valor del input después de eliminar el pedido
-        setInputValue('');
-      })
-      .catch(error => {
-        console.error('Error al eliminar pedidos:', error); // Manejo de errores
-      });
-    }
-
-    async function fetchPedidos() {
-      try {
-        const data = await obtenerDatosPedidos();
-        setPedidos(data);
-      } catch (error) {
-        console.error('Error al obtener los pedidos:', error);
-      }
-    }
-
-    fetchPedidos();
-
-
-
-  }, [inputValue, pedidos]);
-  
-  
-  
-  
-
-  useEffect(() => {
-    const input = document.getElementById('inputBarra');
-    if (input) {
-      input.focus();
-    }
-  }, []); // Enfocar el input cuando se monta el componente
-
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
- 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleGuardarPedidosPorNumeroOrden = (numeroOrden) => {
+    const pedidosFiltrados = pedidos.filter(pedido => pedido.NumOrden === numeroOrden);
+    setPedidosConNumeroOrden(pedidosFiltrados);
+  };
+
+  const handleButtonOnClick = (numeroOrden) => {
+    setNumeroOrdenSeleccionado(numeroOrden);
+    handleGuardarPedidosPorNumeroOrden(numeroOrden);
+    openModal();
+    setNumerosOrdenMostrados(prevState => [...prevState, numeroOrden]);
+    setRowSpanCount(1); // Reiniciar el contador de filas fusionadas
+  };
 
   return (
     <div>
-      <h2>Cola de Pedidos</h2>
-      <input
-        id="inputBarra"
-        type="number"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Ingrese el número de barra"
-      />
-     
+   
       <table style={{ width: '100%' }}>
         <thead>
           <tr>
-            <th>ID</th>
+            {/* <th>ID</th> */}
             <th>Orden</th>
             <th>Cantidad</th>
-            <th>Llaves</th>
+            {/* <th>Llaves</th> */}
             <th>Comentario</th>
             <th>Precio</th>
-            <th>Estado</th>
-            <th>Barra</th>
+            {/* <th>Estado</th> */}
+            {/* <th>Barra</th> */}
             <th>Cliente</th>
             <th>Número de Orden</th>
           </tr>
         </thead>
         <tbody>
-  {pedidos.filter(pedido => pedido.Estado === 1).map((pedido, index) => (
-    <tr key={index}>
-      <td>{pedido.Id_pedidos}</td>
-      <td>{pedido.OrdenTxt}</td>
-      <td>{pedido.Cantidad}</td>
-      <td>{pedido.Llaves}</td>
-      <td>
-                {pedido.Comentario.split('\n').map((linea, i) => (
-                  <div key={i}>{linea}</div>
-                ))}
-              </td>
-      <td>{pedido.Precio}</td>
-      <td>{pedido.Estado}</td>
-      <td>{pedido.Barra}</td>
-      <td>{pedido.Cliente}</td>
-      <td>{pedido.NumOrden}</td>
-    </tr>
-  ))}
-</tbody>
-
+          {pedidos
+            .filter(pedido => pedido.Estado === 1)
+            .map((pedido, index, array) => {
+              const isFirstRowOfGroup = array.findIndex(p => p.NumOrden === pedido.NumOrden) === index;
+              return (
+                <tr key={index}>
+                  {/* <td>{pedido.Id_pedidos}</td> */}
+                  <td>{pedido.OrdenTxt}</td>
+                  <td>{pedido.Cantidad}</td>
+                  {/* <td>{pedido.Llaves}</td> */}
+                  <td>
+                    {pedido.Comentario.split('\n').map((linea, i) => (
+                      <div key={i}>{linea}</div>
+                    ))}
+                  </td>
+                  <td>{pedido.Precio}</td>
+                  {/* <td>{pedido.Estado}</td> */}
+                  {/* <td>{pedido.Barra}</td> */}
+                  <td>{pedido.Cliente}</td>
+                  {isFirstRowOfGroup && (
+                    <td rowSpan={rowSpanCount}>{pedido.NumOrden}</td>
+                  )}
+                  <td>
+                    {isFirstRowOfGroup && (
+                      <button onClick={() => handleButtonOnClick(pedido.NumOrden)}>Ver Detalles</button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <div>
+          <h2>Pedidos con Número de Orden: {numeroOrdenSeleccionado}</h2>
+          <table style={{ width: '100%' }}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Orden</th>
+                <th>Cantidad</th>
+                <th>Llaves</th>
+                <th>Comentario</th>
+                <th>Precio</th>
+                <th>Estado</th>
+                <th>Barra</th>
+                <th>Cliente</th>
+                <th>Número de Orden</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pedidosConNumeroOrden.map((pedido, index) => (
+                <tr key={index}>
+                  <td>{pedido.Id_pedidos}</td>
+                  <td>{pedido.OrdenTxt}</td>
+                  <td>{pedido.Cantidad}</td>
+                  <td>{pedido.Llaves}</td>
+                  <td>
+                    {pedido.Comentario.split('\n').map((linea, i) => (
+                      <div key={i}>{linea}</div>
+                    ))}
+                  </td>
+                  <td>{pedido.Precio}</td>
+                  <td>{pedido.Estado}</td>
+                  <td>{pedido.Barra}</td>
+                  <td>{pedido.Cliente}</td>
+                  <td>{pedido.NumOrden}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={closeModal}>Cerrar</button>
+        </div>
+      </Modal>
     </div>
   );
 }
 
-export default Encargos
+export default Encargos;
+
+
+
+
+
+
